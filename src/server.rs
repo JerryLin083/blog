2cb5b03fb6e48120ddadaf33544257a9cef70b08
JerryLin083 <stabber5760@gmail.com>
2025-04-28 12:42:57 +0800

    add self-signed ssl/tls layer

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
-use tokio::net::TcpListener;
+use std::{net::SocketAddr, path::PathBuf};
+
+use axum_server::tls_rustls::RustlsConfig;
 
 use crate::{db::db_connection, router::router};
 
@@ -6,11 +8,24 @@ pub async fn run() {
     let pool = db_connection().await;
     let router = router(pool).await;
 
-    let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("self_signed_cert")
+            .join("cert.pem"),
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("self_signed_cert")
+            .join("key.pem"),
+    )
+    .await
+    .unwrap();
 
-    tracing::info!("Listening on {}...", listener.local_addr().unwrap());
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000 as u16));
+    tracing::info!("Listening on {}...", addr);
 
-    if let Err(err) = axum::serve(listener, router).await {
-        tracing::error!("Server error: {}", err);
+    if let Err(err) = axum_server::bind_rustls(addr, config)
+        .serve(router.into_make_service())
+        .await
+    {
+        panic!("Server error: {}", err);
     }
 }
