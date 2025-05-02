2f4135227152a75828cc5c076d00b655709f02c0
JerryLin083 <stabber5760@gmail.com>
2025-05-02 10:44:20 +0800

    add solidjs as frontend, and add static file at backend

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+
+export default defineConfig(() => {
+  return {
+    plugins: [solid()],
+    build: {
+      //assetsInlineLimit: 0,
+      outDir: "../backend/static",
+      emptyOutDir: true,
+    },
+  };
+});
